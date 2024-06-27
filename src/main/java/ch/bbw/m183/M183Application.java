1b3c0082332c32af7303e84c6103df206946c919
Marc Wilhelm <73398298+MarcWilhelm@users.noreply.github.com>
2024-06-27 11:22:39 +0200

    Init

---
+++ src/main/java/ch/bbw/m183/M183Application.java
@@ -0,0 +1,13 @@
+package ch.bbw.m183;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M183Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M183Application.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/m183/M183ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.m183;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M183ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
